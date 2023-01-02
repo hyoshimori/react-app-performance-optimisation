6a0666328ea2250afecbb4f96f88a38960fc7ea6
Hiroto Yoshimori <hiroto.y.129@gmail.com>
2023-01-02 23:26:39 +0100

    first setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+// Rerendering of child components
+import React, { useState } from "react";
+import Child from "./Child";
+import "./style/app.css";
 
-function App() {
+const App = () => {
+  console.log("Parent rendered");
+  const [countA, setCountA] = useState(0);
+  const [countB, setCountB] = useState(0);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="parent">
+      <div>
+        <h3>Parent components</h3>
+        <div>
+          <button
+            onClick={() => {
+              setCountA((pre) => pre + 1);
+            }}
+          >
+            ButtonA
+          </button>
+          <span>Update the parent</span>
+        </div>
+
+        {/* Button B starts from here */}
+
+        <div>
+          <button
+            onClick={() => {
+              setCountB((pre) => pre + 1);
+            }}
+          >
+            ButtonB
+          </button>
+          <span>Update the props which is to be passed to Child</span>
+        </div>
+      </div>
+      <div>
+        <p>Number of clicks：{countA}</p>
+      </div>
+      <Child countB={countB} />
     </div>
   );
-}
+};
 
 export default App;
